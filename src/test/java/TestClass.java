18faf07b9c9be52cd6b218fe037e52a8fb568153
Anton Vasylenko <avasylenko@dynamo-kiev.local>
2019-01-21 15:39:19 +0200

    Test Commit 2
    Test

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -13,6 +13,7 @@ public class TestClass {
         driver = new ChromeDriver();
 
         driver.get("https://www.google.com/"); // go to google.com page
+        System.out.println("All went well");
 
         driver.close(); // close your browser
     }
